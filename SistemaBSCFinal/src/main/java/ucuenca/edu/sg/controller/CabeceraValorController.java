e508740ce11b6f3fa7054299ef2ed7eb0929b41d
cuenta4384 <cuenta4384@e93e6a9a-4805-0f17-2781-e56af602058c>
2015-02-02 12:02:52 +0000

    Valor actual controladores

--- SistemaBSCFinal/src/main/java/ucuenca/edu/sg/controller/CabeceraValorController.java
+++ SistemaBSCFinal/src/main/java/ucuenca/edu/sg/controller/CabeceraValorController.java
@@ -3,22 +3,232 @@ package ucuenca.edu.sg.controller;
 import ucuenca.edu.sg.modelo.CabeceraValor;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.el.ELException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartSeries;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartSeries;
+import ucuenca.edu.sg.formula.Formula;
+import ucuenca.edu.sg.formula.Variable;
+import ucuenca.edu.sg.modelo.ComponenteFormula;
 
 @Named("cabeceraValorController")
 @SessionScoped
-public class CabeceraValorController  extends AbstractController<CabeceraValor> implements Serializable {
+public class CabeceraValorController extends AbstractController<CabeceraValor> implements Serializable {
 
     @EJB
     private ucuenca.edu.sg.facade.CabeceraValorFacade ejbFacade;
+    private String formula;
+    //private List<ComponenteFormula> lComponentes;
+    private List<Variable> variables;
+    private List<CabeceraValor> filtroCabeceras;
 
     public CabeceraValorController() {
     }
+    private CartesianChartModel viviendas;
+
+    public CartesianChartModel getViviendas() {
+        return viviendas;
+    }
+
     @PostConstruct
     public void init() {
         super.setFacade(ejbFacade);
+        variables = new ArrayList<>();
+        //createLineModels();
+
+        viviendas = new CartesianChartModel();
+
+        final ChartSeries venta = new ChartSeries("Venta");
+        venta.set("2008", 800);
+        venta.set("2009", 1300);
+        venta.set("2010", 700);
+        venta.set("2011", 500);
+
+        final ChartSeries alquiler = new ChartSeries("Alquiler");
+        alquiler.set("2008", 1200);
+        alquiler.set("2009", 1100);
+        alquiler.set("2010", 1700);
+        alquiler.set("2011", 1900);
+
+        viviendas.addSeries(venta);
+        viviendas.addSeries(alquiler);
     }
+
+    public List<CabeceraValor> getFiltroCabeceras() {
+        return filtroCabeceras;
+    }
+
+    public void setFiltroCabeceras(List<CabeceraValor> filtroCabeceras) {
+        this.filtroCabeceras = filtroCabeceras;
+    }
+
+    public void iniciar() {
+        this.setSelected(new CabeceraValor());
+        this.getSelected().setValorTotal(20.25);
+    }
+
+    @Override
+    public void create() {
+        try {
+            Date d = new Date();
+            this.getSelected().setFecha(d);
+            System.out.println(this.getSelected().getIdIndicador().getFormula());
+            super.create(); //To change body of generated methods, choose Tools | Templates.
+            for (Variable variable : variables) {
+                DetalleValorController dvc = new DetalleValorController();
+                dvc.inicializar(this.getSelected(), variable.getComponenteFormula(), variable.getValor());
+                dvc.create();
+            }
+            anadirMensaje(1);
+        } catch (ELException | NullPointerException e) {
+            anadirMensaje(2);
+        }
+
+    }
+
+    public void mostrarformula() {
+        setFormula(formula);
+        formula = this.getSelected().getIdIndicador().getFormula();
+        List<ComponenteFormula> listaComponentes = this.getSelected().getIdIndicador().getComponenteFormulaList();
+        variables.clear();
+        for (ComponenteFormula Componente : listaComponentes) {
+            Variable v = new Variable(Componente, 0.0);
+            variables.add(v);
+        }
+        setVariables(variables);
+        //setlComponentes(listaComponentes);
+    }
+
+    public List<Variable> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(List<Variable> variables) {
+        this.variables = variables;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+
+    /**
+     * @param formula the formula to set
+     */
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+
+    public double getResultado() {
+        Formula f = new Formula(variables, formula);
+        Double resultado = 0.0;
+        if (f.validar()) {
+            resultado = Double.parseDouble(f.resultado());
+            this.getSelected().setValorTotal(resultado);
+        } else {
+            resultado = 0.0;
+        }
+        return resultado;
+    }
+
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+
+        if (newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Valor Cambiado", "a: " + newValue + ", Resultado: " + getResultado());
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public void anadirMensaje(int tipo) {
+        FacesMessage msg = null;
+        switch (tipo) {
+            case 1:
+                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Almacenado", "Se ha guardado con exito");
+                break;
+            case 2:
+                msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Actualice la pagina e ingrese daos coherentes");
+                break;
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    private LineChartModel lineModel2;
+
+    public LineChartModel getLineModel2() {
+        return lineModel2;
+    }
+
+    private void createLineModels() {
+        lineModel2 = initCategoryModel();
+        lineModel2.setTitle("Grafica");
+        lineModel2.setLegendPosition("e");
+        lineModel2.setShowPointLabels(true);
+        lineModel2.getAxes().put(AxisType.X, new CategoryAxis("Fechas"));
+        Axis yAxis = lineModel2.getAxis(AxisType.Y);
+        yAxis.setLabel("Valor Total");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+    }
+
+    private LineChartModel initCategoryModel() {
+        List<CabeceraValor> l = super.getItems();
+
+        HashMap<String, String> hm = new HashMap();
+        for (CabeceraValor l1 : l) {
+            hm.put(l1.getIdIndicador().getNombreIndicador(), l1.getIdIndicador().getNombreIndicador());
+        }
+
+        LineChartModel model = new LineChartModel();
+        Iterator it = hm.entrySet().iterator();
+
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+//        
+//        while (it.hasNext()) {
+//            Map.Entry e = (Map.Entry) it.next();
+//            System.out.println(e.getKey() + " " + e.getValue());
+//            ChartSeries cs = new ChartSeries();
+//            cs.setLabel(e.getKey() + "");
+//            for (CabeceraValor l1 : l) {
+//                if (e.getKey().equals(l1.getIdIndicador().getNombreIndicador())) {
+//                    Calendar calendar = Calendar.getInstance();
+//                    calendar.setTime(l1.getFecha());
+//                    String mes_ = (calendar.get(Calendar.MONTH) + 1) + "";
+//                    cs.set(mes_, l1.getValorTotal());
+//                }
+//            }
+//            model.addSeries(cs);
+//        }
+        model.addSeries(boys);
+        return model;
+    }
+
 }
